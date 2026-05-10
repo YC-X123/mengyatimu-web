0f75fef155fcb3345c9baee9215d86e23a745c71
YC-X123 <2010967007@qq.com>
2026-05-10 21:28:22 +0800

    创建store/index.js文件

---
+++ client/src/store/index.js
@@ -0,0 +1,12 @@
+﻿import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './authSlice'
+import quizReducer from './quizSlice'
+import storeReducer from './storeSlice'
+
+export default configureStore({
+  reducer: {
+    auth: authReducer,
+    quiz: quizReducer,
+    store: storeReducer
+  }
+})
