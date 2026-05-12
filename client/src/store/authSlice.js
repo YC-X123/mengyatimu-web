0bebda9607a0ab57b548c0f946f80575344ea16b
YC-X123 <2010967007@qq.com>
2026-05-12 21:53:05 +0800

    修复authSlice和API配置

--- client/src/store/authSlice.js
+++ client/src/store/authSlice.js
@@ -1,7 +1,7 @@
 ﻿import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://mengyatimu-web-production.up.railway.app'
+const API_URL = 'https://mengyatimu-web-production.up.railway.app'
 
 const initialState = {
   user: null,
